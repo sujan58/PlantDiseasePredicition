88dd40a383bcca9a42ed86b7bb661ec52e46f395
sujan58 <rjrsujan>
2026-08-21 09:20:20 +0530

    ADD EfficientNetB0 plant disease prediction

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PlantDiseaseDetector from "./components/PlantDiseaseDetector";
+
+
+function App() {
+  return (
+    <PlantDiseaseDetector />
+  );
+}
+
+
+export default App;
